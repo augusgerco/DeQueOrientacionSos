5c9feba3715f80b74adc0e33dbbdbe745af304a1
hanono09 <fedehanono@gmail.com>
2025-04-28 23:30:52 -0300

    FlotaDeTaxis COMPELTADO

---
+++ Assets/Scripts/FlotaDeTaxis.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlotaDeTaxis : MonoBehaviour
+{
+    public int cantidadDeTaxis;
+    public int cantidadDiasTrabajados;
+
+    int litrosPorDia = 6;
+    int precioLitroCombustible = 130;
+
+    float precioTotalCombustible = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cantidadDiasTrabajados < 5){
+            Debug.Log("Error. Hay un minimo de 5 dias de trabajo");
+            return;
+        }
+        if(cantidadDeTaxis < 1){
+            Debug.Log("Error. Hay un minimo de 1 taxi por flota");
+            return;
+        }
+
+        precioTotalCombustible = precioTotalCombustible + litrosPorDia * cantidadDeTaxis * cantidadDiasTrabajados * precioLitroCombustible;
+
+    // supera los 100 litros
+        if(cantidadDiasTrabajados * cantidadDeTaxis * litrosPorDia > 100){
+            precioTotalCombustible = precioTotalCombustible * 0.80f;
+        }
+
+        Debug.Log("Una flota de " + cantidadDeTaxis + " unidades trabajando durante "  + cantidadDiasTrabajados + " días implicará un gasto de " + precioTotalCombustible + " pesos en concepto de combustible");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
